ed91813d4d237612ff64572e99ab36751bb5869e
Amanda Albuquerque <amandlei@hotmail.com>
2018-02-07 19:42:40 -0200

    add Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ const Header = () => (
   <header>
     <nav>
       <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/product'>Products</Link></li>
+        <li>Music shop</li>
+        <li><Link to='/product'>Products<Link></li>
         <li><Link to='/cart'>Carrinho</Link></li>
       </ul>
     </nav>
