dc609ee0b951034757046201e02753c9eb28db88
Amanda Albuquerque <amandlei@hotmail.com>
2018-02-14 23:26:04 -0200

    indent code

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ImageAlbum from '../images/album-photo.jpg';
+
 const Product = ({product, children, onClick}) => (
   <div className="item" onClick={onClick}>
 		<h2 className="titulo">{product.name} - {product.vendor.name}</h2>
 		<div className="detalhes-produto">
 			<div className="detalhes">
-				<img alt={product.name} src={require("../images/album-photo.jpg")} />
+				<img alt={product.name} src={ImageAlbum} />
 				<strong className="valor">R$ {(product.price/100).toFixed(2).replace('.', ',')}</strong>
 			</div>
 			<div className="detalhes-album">
